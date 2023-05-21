df8effec1460e87742670bf71d0191f818cc09a3
Eleazar Nhamuave <eleazarovich176@gmail.com>
2023-05-21 12:33:29 +0200

    prep for servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#this script sets up web servers for the deployment of web_static
+
+sudo apt-get update -y && sudo apt-get upgrade -y
+#sudo apt-get install nginx -y
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+
+sudo service nginx restart
